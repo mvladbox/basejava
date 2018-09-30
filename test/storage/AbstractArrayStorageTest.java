0cee1ecb19c93d98ddff04cbb087169c3a286f30
Влад Михайлов <vladbox@gmail.com>
2018-09-30 11:31:56 +0700

    HW-04 Edits by remarks

--- test/storage/AbstractArrayStorageTest.java
+++ test/storage/AbstractArrayStorageTest.java
@@ -4,12 +4,11 @@ import exception.ExistsStorageException;
 import exception.StorageException;
 
 import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 import model.Resume;
 import exception.NotExistsStorageException;
-
-import java.lang.reflect.Field;
 
 public abstract class AbstractArrayStorageTest {
 
@@ -18,36 +17,59 @@ public abstract class AbstractArrayStorageTest {
     private static final String UUID_1 = "uuid1";
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
+    private static final String UUID_DUMMY = "dummy";
 
-    public AbstractArrayStorageTest(Storage storage) {
+    private static final Resume RESUME_1 = new Resume(UUID_1);
+    private static final Resume RESUME_2 = new Resume(UUID_2);
+    private static final Resume RESUME_3 = new Resume(UUID_3);
+    private static final Resume RESUME_DUMMY = new Resume(UUID_DUMMY);
+
+    protected AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
     }
 
     @Before
     public void setUp() throws Exception {
         storage.clear();
-        storage.save(new Resume(UUID_3));
-        storage.save(new Resume(UUID_1));
-        storage.save(new Resume(UUID_2));
+        storage.save(RESUME_1);
+        storage.save(RESUME_2);
+        storage.save(RESUME_3);
+    }
+
+    @Test
+    public void size() throws Exception {
+        assertSize(3);
     }
 
     @Test
     public void clear() throws Exception {
         storage.clear();
-        assertEquals(0, storage.size());
+        assertSize(0);
     }
 
     @Test
     public void save() throws Exception {
-        Resume resume = new Resume("trial");
-        storage.save(resume);
-        assertEquals(4, storage.size());
-        assertSame(resume, storage.get(resume.getUuid()));
+        storage.save(RESUME_DUMMY);
+        assertSize(4);
+        assertResumeExists(RESUME_DUMMY);
     }
 
     @Test(expected = ExistsStorageException.class)
-    public void saveDuplicate() throws Exception {
-        storage.save(new Resume(UUID_2));
+    public void saveExists() throws Exception {
+        storage.save(RESUME_2);
+    }
+
+    @Test(expected = StorageException.class)
+    public void saveOverflow() throws Exception {
+        storage.clear();
+        try {
+            for (int i = 0; i < AbstractArrayStorage.RESUME_MAX_COUNT; i++) {
+                storage.save(new Resume());
+            }
+        } catch (StorageException e) {
+            fail("Наверно, закончилась память!");
+        }
+        storage.save(new Resume());
     }
 
     @Test
@@ -60,57 +82,44 @@ public abstract class AbstractArrayStorageTest {
 
     @Test(expected = NotExistsStorageException.class)
     public void updateNotExists() throws Exception {
-        storage.update(new Resume("dummy"));
+        storage.update(RESUME_DUMMY);
     }
 
     @Test(expected = NotExistsStorageException.class)
     public void delete() throws Exception {
         storage.delete(UUID_2);
-        assertEquals(2, storage.size());
+        assertSize(2);
         storage.get(UUID_2);
+    }
+
+    @Test(expected = NotExistsStorageException.class)
+    public void deleteNotExists() throws Exception {
+        storage.delete(UUID_DUMMY);
     }
 
     @Test
     public void get() throws Exception {
-        Resume resume = storage.get(UUID_2);
-        assertEquals(UUID_2, resume.getUuid());
+        assertResumeExists(RESUME_2);
     }
 
     @Test(expected = NotExistsStorageException.class)
     public void getNotExists() throws Exception {
-        storage.get("dummy");
-    }
-
-    @Test
-    public void size() throws Exception {
-        assertEquals(3, storage.size());
+        storage.get(UUID_DUMMY);
     }
 
     @Test
     public void getAll() throws Exception {
         assertEquals(storage.size(), storage.getAll().length);
+        assertSame(RESUME_1, storage.get(UUID_1));
+        assertSame(RESUME_2, storage.get(UUID_2));
+        assertSame(RESUME_3, storage.get(UUID_3));
     }
 
-    @Test(expected = NotExistsStorageException.class)
-    public void getNotExist() throws Exception {
-        storage.get("dummy");
+    private void assertSize(int size) {
+        assertEquals(size, storage.size());
     }
 
-    @Test(expected = StorageException.class)
-    public void overflow() throws Exception {
-        storage.clear();
-        Field field = AbstractArrayStorage.class.getDeclaredField("RESUME_MAX_COUNT");
-        field.setAccessible(true);
-        final int LIMIT = field.getInt(storage);
-
-        try {
-            for (int i = 0; i < LIMIT; i++) {
-                storage.save(new Resume());
-            }
-        } catch (Exception e) {
-            fail("Наверно, закончилась память!");
-        }
-
-        storage.save(new Resume());
+    private void assertResumeExists(Resume resume) {
+        assertSame(resume, storage.get(resume.getUuid()));
     }
 }
