52a56f6513f1583f93c89b255078906425d99d67
mvladbox <vladbox@gmail.com>
2018-12-06 16:55:51 +0700

    HW-12 Stream API

---
+++ src/main/java/ru/vlad/app/MainStream.java
@@ -0,0 +1,40 @@
+package ru.vlad.app;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MainStream {
+    public static void main(String[] args) {
+        int[] values1 = {1, 2, 3, 3, 2, 3};
+        int[] values2 = {9, 8};
+        List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> list2 = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+
+        System.out.println("\n1. minValue");
+        System.out.println(Arrays.toString(values1) + "  ->  " + minValue(values1));
+        System.out.println(Arrays.toString(values2) + "  ->  " + minValue(values2));
+        System.out.println("\n2. oddOrEven");
+        System.out.println(list1 + " (sum " + list1.stream().mapToInt(x -> x).sum() + ")  ->  " + oddOrEven(list1));
+        System.out.println(list2 + " (sum " + list2.stream().mapToInt(x -> x).sum() + ")  ->  " + oddOrEven(list2));
+    }
+
+    private static int minValue(int[] values) {
+        AtomicInteger index = new AtomicInteger(0);
+        return Arrays.stream(values)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .distinct()
+                .mapToInt((v) -> v *= (int) Math.pow(10, index.getAndIncrement()))
+                .sum();
+    }
+
+    private static List<Integer> oddOrEven(List<Integer> integers) {
+        return Stream.of(integers.stream().mapToInt(x -> x).sum() % 2)
+                .flatMap((x) -> integers.stream().filter((i) -> (i % 2 != x)))
+                .collect(Collectors.toList());
+    }
+}
